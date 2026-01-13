d2450e43ad215acefe365f612d7e00392863aebf
yamusang <ttobiyam@naver.com>
2026-01-13 17:04:58 +0900

    change files

--- demo/src/test/java/com/yams/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/yams/demo/DemoApplicationTests.java
@@ -1,13 +1,12 @@
 package com.yams.demo;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class DemoApplicationTests {
-
-	@Test
+	@org.junit.jupiter.api.Test
 	void contextLoads() {
 	}
-
 }
